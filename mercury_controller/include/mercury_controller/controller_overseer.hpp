f3f61723844eb346a297e217510cd17bc3ac03ca
John <jjdarnley1@gmail.com>
2026-03-02 21:22:16 -0500

    readConfig finished, added helper functions and eigen naming conventions from robot_class.cpp from the simulator

--- mercury_controller/include/mercury_controller/controller_overseer.hpp
+++ mercury_controller/include/mercury_controller/controller_overseer.hpp
@@ -19,72 +19,62 @@
 #define PARAMETER_SCALE 1000000
 
 
+/*
+    Helper function to make a yaml node into a vector, used for thrusters
+*/
+template<typename T>
+T getYamlNodeAs(const YAML::Node& n, const std::vector<std::string>& keywords)
+{
+    if(keywords.empty())
+    {
+        throw std::runtime_error("getYamlNodeAs() requires at least one keyword.");
+    }
+
+    YAML::Node node = YAML::Clone(n);
+    
+    try
+    {
+        for(std::string s : keywords)
+        {
+            node = node[s];
+        }
+
+        return node.as<T>();
+    } catch(YAML::Exception& e)
+    {
+        std::string msg = "Failed to parse value at tag " + keywords[0];
+        for(size_t i = 1; i < keywords.size(); i++)
+        {
+            msg +=  " -> " + keywords[i];
+        }
+        
+        msg += ": " + std::string(e.what());
+        throw std::runtime_error(msg);
+    }
+}
+
+/*
+    Helper function to quickly go to Eigen 3d vector object
+*/
+std2v3d(std::vector<double> stdVect)
+{
+    return v3d(stdVect[0], stdVect[1], stdVect[2]);
+}
+
+
+
 class ControllerOverseer : public rclcpp::Node {
+
+    using v3d = Eigen::Vector3d;
+    using v4d = Eigen::Vector4d;
+    using vXd = Eigen::VectorXd;
+    using m3d = Eigen::Matrix3d;
+    using mXd = Eigen::MatrixXd;
+    using quat = Eigen::Quaterniond;    
 
     using string = std::string;
 
-    bool waitingOnInit;
-
-    int escPowerStopsLow, escPowerStopsHigh;
-
-    YAML::Node configTree;
-    YAML::Node autoffTree;
-
-    YAML::Node thrusterInfo;
-    YAML::Node com;
-
-    bool activeThrusters[8];
-    bool submergedThrusters[8];
-    int thrusterWeights[8];
-
-    int thrusterMode;
-
-    string autoffConfigPath;
-
-    std::shared_ptr<SimulinkModelClass> completeController;
-
-    rclcpp::Subscriber</*DShotPartialTelemetry*/>::SharedPtr thrusterTelemetry;
-
-    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr motionEnabledPub;
-
-    rclcpp::Service<rcl_interfaces::srv::SetParameters>::SharedPtr setThrusterSolverParams;
-
-    rclcpp::Subscriber<std_msgs::msg::Int16>::SharedPtr thrusterMode;
-
-    rclcpp::Subscriber<nav_msgs::msg::Odometry>::SharedPtr odom;
-
-    rclcpp::Subscriber<geometry_msgs::msg::Twist>::SharedPtr ffAutoTune;
-    
-    rclcpp::Publisher<std_msgs::msg::Int32MultiArray>::SharedPtr weightsPub;
-
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr ffPub;
-
-    rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr reInitPub;
-
-    rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr setTeleop;
-
-    rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr setTeleop;
-
-    tf2_ros::Buffer::SharedPtr tfBuffer;
-
-    tf2_ros::TransformListener::SharedPtr tfListener;
-
-    string tfNamespace;
-
-    rclcpp::Time startTime;
-
-    rclcpp::TimerBase::SharedPtr updateTimer;
-
-    rclcpp::TimerBase::SharedPtr weightTimer;
-
-    bool enabled;
-    bool publishingFF;
-
-    rclcpp::TimerBase::SharedPtr escPowerCheckTimer;
-
-    //parameters
-    string robotName, configPath, thrusterSolverName;
-    bool writeAutoFF;
+    public:
 
     ControllerOverseer() : Node("controller_overseer") {
 
@@ -103,7 +93,6 @@ class ControllerOverseer : public rclcpp::Node {
 
         declare_parameter(FF_PUBLISH_PARAM, false);
 
-
         setConfigPath();
         readConfig();
 
@@ -116,40 +105,28 @@ class ControllerOverseer : public rclcpp::Node {
         }
 
             
-        thrusterTelemetry = create_subscription</*DShotPartialTelemetry*/>("/state/thrusters/telemetry", rclcpp::SystemDefaultsQoS(), /*bind callback*/);
-
-        motionEnabledPub = create_publisher<std_msgs::msg::Bool>("controller/motion_enabled", rclcpp::SystemDefaultsQoS());
-
+        thrusterTelemetry = create_subscription</*DShotPartialTelemetry*/>("/state/thrusters/telemetry", rclcpp::SystemDefaultsQoS(), /*bind callback*/);        
         setThrusterSolverParams = create_client<rcl_interfaces::srv::SetParameters>(thrusterSolverName + "/set_parameters");
 
+        motionEnabledPub = create_publisher<std_msgs::msg::Bool>("controller/motion_enabled", rclcpp::SystemDefaultsQoS());
         thrusterMode = create_subscription<std_msgs::msg::Int16>("thrusterSolver/thrusterState", rclcpp::SystemDefaultsQoS(), /*bind callback*/);
-
         odom = create_subscription<nav_msgs::msg::Odometry>("odometry/filtered", rclcpp::SystemDefaultsQoS(), /*callback*/);
-
         ffAutoTune = create_subscription<geometry_msgs::msg::Twist>("ff_auto_tune", rclcpp::SystemDefaultsQoS(), /*callback*/);
-        
         weightsPub = create_publisher<std::msgs::Int32MultiArray>("controller/solver_weights", rclcpp::SystemDefaultsQoS());
-
         ffPub = create_publisher<geometry_msgs::msg::Twist>("controller/FF_body_force", rclcpp::SystemDefaultsQoS());
-
         reInitPub = create_publisher<std_msgs::msg::Empty>("controller/re_init_accumulators", rclcpp::SystemDefaultsQoS());
 
         setTeleop = create_client<std_srvs::srv::SetBool>("setTeleop", /*callback*/);
 
         tfBuffer = std::make_shared<tf2_ros::Buffer>(get_clock());
-
         tfListener = std::make_shared<tf2_ros::TransformListener>(*tfBuffer);
 
-        tfNamespace = "talos";      //get_parameter("robot").as_string()??
+        tfNamespace = get_parameter("robot").as_string();
 
         updateTimer = create_wall_timer(std::chrono_literals::1s, /*callback*/);
-
-        weightTimer = create_wall_timer(std::chrono_literals::1s, /*callback*/);
-        
+        weightTimer = create_wall_timer(std::chrono_literals::1s, /*callback*/); 
 
     }
-
-    public:
 
     //construct the complete controller class using the pointer for "this" instance
     void bind_pointers(std::shared_ptr<ControllerOverseer> node){
@@ -160,7 +137,7 @@ class ControllerOverseer : public rclcpp::Node {
     /*
     Yaml traversal -- get recursed
     */
-    void traversal(std::vector<std::pair<std::string,int64_t>>& ints, std::vector<std::pair<std::string,bool>>& bools, std::vector<std::pair<std::string,std::vector<int64_t>>> & arrays, const YAML::Node& tree, const std::string path){
+    void traversal(std::vector<std::pair<std::string,double>>& ints, std::vector<std::pair<std::string,bool>>& bools, std::vector<std::pair<std::string,std::vector<double>>> & arrays, const YAML::Node& tree, const std::string path){
         switch(tree.Type()){
             case YAML::NodeType::Map:
                 {
@@ -180,12 +157,12 @@ class ControllerOverseer : public rclcpp::Node {
                 }
             case YAML::NodeType::Sequence:{
 
-                std::vector<int64_t> array;
+                std::vector<double> array;
 
                 for(const auto& item : tree){
                     if(item.IsScalar()){
                         try{
-                            int64_t changed = item.as<double>()*PARAMETERSCALE;
+                             changed = item.as<double>()*PARAMETERSCALE;
                             array.push_back(changed);
                         }catch (const YAML::BadConversion& e){
                             RCLCPP_ERROR(get_logger(), "%s: %s not read properly", path, item.Tag());
@@ -200,7 +177,7 @@ class ControllerOverseer : public rclcpp::Node {
             }
 
             case YAML::NodeType::Scalar:{
-                int64_t num = 0;
+                int num = 0;
                 bool boolean;
                 try{
                     num = tree.as<double>() * PARAMETERSCALE;
@@ -238,7 +215,18 @@ class ControllerOverseer : public rclcpp::Node {
     void readConfig(){
         try{
             configTree = YAML::LoadFile(configPath);
+            thrusterInfo = configTree["thrusters"];
+            com = getYamlNodeAs<std::vector<double>>(configTree, {"com"});
+
+            baseWrench = getYamlNodeAs<std::vector<double>>(configTree, {"controller", "feed_forward", "base_wrench"});
             traversal(completeController.intV, completeController.boolV, completeController.arrayV, configTree, "");
+
+            auto thrusterSolverInfo = configTree["thruster_solver"];
+            defaultWeight = getYamlNodeAs<double>(thrusterSolverInfo, {"default_weight"});
+            surfaceWeight = getYamlNodeAs<double>(thrusterSolverInfo, {"surfaced_weight"});
+            disabledWeight = getYamlNodeAs<double>(thrusterSolverInfo, {"disable_weight"});
+            lowDowndraftWeight = getYamlNodeAs<double>(thrusterSolverInfo, {"low_downdraft_weight"});
+
         }catch(YAML::BadFile &e){
             RCLCPP_ERROR(get_logger(), "Cannot open config file at %s", configPath.c_str());
         }
@@ -251,7 +239,66 @@ class ControllerOverseer : public rclcpp::Node {
             RCLCPP_ERROR(get_logger(), "Cannot open config file at %s", autoffconfigPath.c_str());
         }
 
+
+
     }
+
+
+    private:
+
+    bool waitingOnInit;
+
+    int escPowerStopsLow, escPowerStopsHigh;
+
+    YAML::Node configTree;
+    YAML::Node autoffTree;
+
+    Yaml::Node thrusterInfo;
+    YAML::Node com;
+
+    bool activeThrusters[8];
+    bool submergedThrusters[8];
+    int thrusterWeights[8];
+
+    int thrusterMode;
+
+    string autoffConfigPath;
+
+    std::shared_ptr<SimulinkModelClass> completeController;
+
+    rclcpp::Subscriber</*DShotPartialTelemetry*/>::SharedPtr thrusterTelemetry;
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr motionEnabledPub;
+    rclcpp::Service<rcl_interfaces::srv::SetParameters>::SharedPtr setThrusterSolverParams;
+
+    rclcpp::Subscriber<std_msgs::msg::Int16>::SharedPtr thrusterMode;
+    rclcpp::Subscriber<nav_msgs::msg::Odometry>::SharedPtr odom;
+    rclcpp::Subscriber<geometry_msgs::msg::Twist>::SharedPtr ffAutoTune;
+    rclcpp::Publisher<std_msgs::msg::Int32MultiArray>::SharedPtr weightsPub;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr ffPub;
+    rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr reInitPub;
+
+    rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr setTeleop;
+
+    tf2_ros::Buffer::SharedPtr tfBuffer;
+    tf2_ros::TransformListener::SharedPtr tfListener;
+
+    string tfNamespace;
+
+    rclcpp::Time startTime;
+    rclcpp::TimerBase::SharedPtr updateTimer;
+    rclcpp::TimerBase::SharedPtr weightTimer;
+    rclcpp::TimerBase::SharedPtr escPowerCheckTimer;
+
+    bool enabled;
+    bool publishingFF;
+
+    std::vector<double> baseWrench;
+
+    double defaultWeight, surfaceWeight, disabledWeight, lowDowndraftWeight;
+
+    //parameters
+    string robotName, configPath, thrusterSolverName;
+    bool writeAutoFF;
 
     
 
